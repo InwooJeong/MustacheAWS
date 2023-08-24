9b8943c00e1389ba1432c709935ab88c6f8abde7
inu <sinth92@hotmail.com>
2023-08-24 14:24:43 +0900

    Added update, delete

--- src/main/java/org/example/springboot/service/posts/PostsService.java
+++ src/main/java/org/example/springboot/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package org.example.springboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.domain.posts.Posts;
 import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.dto.PostsListResponseDto;
 import org.example.springboot.dto.PostsResponseDto;
 import org.example.springboot.dto.PostsSaveRequestDto;
 import org.example.springboot.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -35,6 +39,23 @@ public class PostsService {
       .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
     return new PostsResponseDto(entity);
+  }
+
+  @Transactional(readOnly = true)   //트랜잭션 범위는 유지하되 조회 기능만 남김 -> 조회 속도 개선
+  public List<PostsListResponseDto> findAllDesc(){
+    return postsRepository.findAllDesc().stream()
+      .map(PostsListResponseDto::new)   //.map(posts -> new PostsListResponseDto(posts))
+      .collect(Collectors.toList());
+  }
+  
+  @Transactional
+  public void delete(Long id){
+    Posts posts = postsRepository.findById(id)
+      .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+    //JpaRepository에서 지원하는 delete
+    //Entity를 파라미터로 삭제하거나 deleteById 메소드를 이용, id로 삭제
+    postsRepository.delete(posts);
   }
 
 }
